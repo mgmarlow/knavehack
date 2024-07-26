f1c41c7614f697568be9c5a9338390d78a4be004
Graham Marlow <graham@mgmarlow.com>
2024-07-25 17:00:31 -0700

    Table of contents and anchor links

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,24 @@
 const esbuild = require("esbuild");
+const markdownItAnchor = require("markdown-it-anchor");
+const pluginTOC = require("eleventy-plugin-toc");
 const tableroll = require("./_11ty/shortcodes/tableroll");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.amendLibrary("md", (lib) => lib.use(markdownItAnchor));
   eleventyConfig.addWatchTarget("assets/js/");
+
+  eleventyConfig.addPlugin(pluginTOC, {
+    ul: true,
+  });
+
+  eleventyConfig.addPassthroughCopy("public");
 
   eleventyConfig.on("afterBuild", () => {
     return esbuild.build({
       entryPoints: ["assets/js/application.js"],
       outdir: "_site/assets/js/",
       minify: process.env.ELEVENTY_ENV === "production",
-      bundle: true
+      bundle: true,
     });
   });
 
